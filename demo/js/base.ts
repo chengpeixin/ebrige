4ae9159923fb1666804572b91488e1a28e4b057b
xinpeicheng <xinpeicheng@yeah.net>
2019-06-21 11:45:44 +0800

    feat(版本发布，从npm可以): no
    no

--- demo/js/base.ts
+++ demo/js/base.ts
@@ -1,14 +1,13 @@
-import ebrige from './../../src/ebrige'
+import ebrige from 'ebrige'
 
 window.onload = function () {
     // const btn: HTMLBodyElement = document.querySelector('.share')
     const btn2: HTMLBodyElement = document.querySelector('.link')
     btn2.onclick = function () {
-        ebrige.dispatch('getName').then(res => {
-            console.log('promise调用')
+        ebrige.dispatch('getUserInfo').then(res => {
             alert(JSON.stringify(res))
         }).catch(e => {
-            alert(JSON.stringify(e))
+
         })
     }
 }
